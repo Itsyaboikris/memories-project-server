32bb2a3c5cbcc1c5a7036ef1d87fc246184eb156
itsyaboikris <kristoff_riley@outlook.com>
2021-07-15 09:49:21 -0400

    create posts function added

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,15 +6,20 @@ export const getPosts = async (req, res) => {
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
     } catch (e) {
-        res.status(404).json({message: e.message})
+        res.status(404).json({message: e.message});
     }
 };
 
-export const createPosts = (req, res) => {
+export const createPosts = async (req, res) => {
     try {
+        const post = req.body;
+
+        const newPost = new PostMessage(post);
+
+        await newPost.save();
+        res.status(201).json(newPost);
 
     } catch (e) {
-
+        res.status(409).json({message: e.message});
     }
-    res.send('Post created!')
 };
