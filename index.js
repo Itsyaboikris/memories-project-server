ca9a3190cc93b94a1fb4182142b1709ebd4f496d
itsyaboikris <kristoff_riley@outlook.com>
2021-07-15 09:13:28 -0400

    server set up

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l51qb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, () => console.log(chalk.green(`Server running on port: ${PORT}`))))
+    .catch((error)=> console.log(chalk.red("Error connecting to database \n" + error.message)));
+
+mongoose.set('useFindAndModify', false);
